878ce638a6f31d95f017f9e395d4e3c054521777
Natalie J <natsetc@gmail.com>
2019-06-02 02:34:36 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+  
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+  
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+  
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+  
+  def feed_fish
+    @pets[:fishes].each {|f| f.mood = "happy"}
+  end
+  
+  def sell_pets
+    @pets.each do |type, pets| 
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+    @pets.each {|type, pets| pets.clear}
+  end
+  
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
 end
